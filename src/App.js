e47c3c4f4d0e87a83ec7dc788ab69e01a4c6ed87
Sujata-123 <sujata672singh@gmail.com>
2021-06-08 10:23:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import TodoListItem from './Todoapplication/TodoListItem';
 const App = () => {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
   return (
     <div>
-    <div>
-      <Todo1
-        input={input}
-        setInput={setInput}
+      <div>
+        <Todo1
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          setEditTodo={setEditTodo}
+        />
+      </div>
+      <TodoListItem
         todos={todos}
         setTodos={setTodos}
+        setEditTodo={setEditTodo}
       />
-      </div>
-      <TodoListItem todos={todos} setTodos={setTodos}/>
     </div>
   );
 };
