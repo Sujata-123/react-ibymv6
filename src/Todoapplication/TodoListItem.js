fd466a448340c9cca6e285ba9d358c6342c4181f
Sujata-123 <sujata672singh@gmail.com>
2021-06-08 10:37:10 +0530

    1 file modified

--- src/Todoapplication/TodoListItem.js
+++ src/Todoapplication/TodoListItem.js
@@ -1,5 +1,24 @@
 import React from 'react';
 const TodoListItem = ({ todos, setTodos }) => {
+  const handleComplete = todo => {
+    setTodos(
+      todos.map(item => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleEdit = ({ id }) => {
+    const findTod = todos.find(todos => todo.id == id);
+    setEditTodo(findTod);
+  };
+
+  const handleDelete = ({ id }) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
   return (
     <div>
       {todos.map(todo => {
@@ -9,6 +28,12 @@ const TodoListItem = ({ todos, setTodos }) => {
             value={todo.title}
             onChange={event => event.preventDefault()}
           />
+          <div>
+            <button onClick={() => handleComplete(todo)}>Complete</button>
+
+            <button onClick={() => handleEdit(todo)}>Edit</button>
+            <button onClick={() => handleDelete(todo)}>Delete</button>
+          </div>
         </li>;
       })}
     </div>
